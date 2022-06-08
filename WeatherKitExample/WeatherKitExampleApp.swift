a3ae35967937bcfd870648788f0d36aaa2567cb0
Tomohiro Tashiro <tashiro@wni.com>
2022-06-08 11:11:24 +0900

    Initial Commit

---
+++ WeatherKitExample/WeatherKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitExampleApp.swift
+//  WeatherKitExample
+//
+//  Created by Tomohiro Tashiro on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
